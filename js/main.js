bc19ee3fab458afe3a52d50b9a14ea4fba33e502
Lucas S Eggers <l.eggers91@gmail.com>
2014-10-02 23:18:02 -0300

    instructions should make more sense now

--- js/main.js
+++ js/main.js
@@ -1,92 +1,111 @@
 window.onload = function () {
+    // Artboad setup:
+    // This works by layering two <svg>s on top of each other. One is the
+    // artboard, the other is the template. In SVG, every node is a layer with
+    // conflicts resolved from top to bottom. If you want that to be useful, you
+    // have to get good at using svg's <g> node.
     Snap.load('assets/template-v11.1.svg', function (fragment) {
 
         var getDimension = function (fragment, dimensionName) {
             var value = fragment.node.firstElementChild.attributes.getNamedItem(dimensionName).value;
             return parseFloat(value);
         };
-        w = getDimension(fragment, "width");
-        h = getDimension(fragment, "height");
+        width = getDimension(fragment, "width");
+        height = getDimension(fragment, "height");
 
-        // First SVG. Needs to be declared first to be below template.
-        art = Snap(w, h)
+        // First SVG. Needs to be added to the DOM first to be below the template.
+        art = Snap(width, height)
             .attr({
                 id: 'draw',
                 style:"position: absolute;left: 0;"
             });
 
-        // Now we add template SVG to DOM.
-        s = Snap(w, h)
+        // Now we add template SVG to the DOM.
+        s = Snap(width, height)
             .attr({ style:"position: absolute;left: 0;" })
             .append(fragment);
 
-        draw(art, w, h);
+        draw(art, width, height);
     });
 
-    // Here's where you get to play with the canvas! You can also enter these things
-    // in the web console to have the same effect (though make sure you use the
-    // art variable instead of b!). We use snap.svg.js to make these shapes (for
-    // now; the library could be substituted provided it generates an SVG). Here
-    // are its docs: http://snapsvg.io/docs.
+    // Here's where you get to play with the artboard! We use snap.svg.js to
+    // make these  shapes (for now; the library could be substituted provided it
+    // generates an SVG). Here are its docs: http://snapsvg.io/docs. You also
+    // will likely want to know about SVG. Normally when I want to know I just
+    // Google or cmd + F on http://www.w3.org/TR/SVG/paths.html or other SVG docs.
 
-    // We could make this into a full-blown webapp by using http://ace.c9.io/#nav=about
-    // and http://repl.it/ and getting SVGs from submitted code, or something.
-    // For now, it's enough for us to enjoy.
+    // Could be made into a full-blown webapp by using http://ace.c9.io/#nav=about
+    // and http://repl.it/ and getting SVGs from post data...?
 
     // WHEN YOU ARE READY TO SAVE YOUR DESIGN:
-    // 1) In the web console type art.toString(). This will spit out an SVG string.
-    // 2) Copy-paste that SVG string into a blank file, name it something that makes sense.
-    // 3) Send it to Bombsheller to be imported to Illustrator!
-    // NOTE: The exported SVG can be dragged directl into Artemix!
-    function draw (b, w, h) {
-        // Background: a square that covers the whole area with a gradient fill.
-        // Notice that we can now edit this after we've loaded the template. Sweet!
-        // You can even do stuff in your console! You have access to any variables
-        // not proceeded with the var declaration (i.e. almost all of them right now).
-        square = b.rect(0, 0, w, h);
-        topToBottom = b.gradient(Snap.format("L({x1}, {y1}, {x2}, {y2}){c1}-{c2}", {
-            x1: 0,
-            y1: 0,
-            x2: 0,
-            y2: h,
-            c1: "#e6ab5a",
-            c2: "#51388F"
+    // 1) Click the "Download SVG" button waaaaay out to the right.
+    // 2) Cmd + S that sucker. Change the filename end to .svg. Accept the prompt.
+    // 3) (Optional) Test your design by dragging it directly into Artemix. If
+    //    it looks wonky you might have to add a white background.
+    // 4) Tweak some variables for some variants (or something), repeat 1-3.
+    //    Make sure you save a copy of the draw() method in the folder with your
+    //    design! Wouldn't want to lose that ;-)
+    // 5) Submit @ http://bombsheller.com/submit-designs.html
+    function draw (svg, width, height) {
+
+        // Start by making a couple of gradients because vector gradients rock.
+        // Notice that we can now edit this after we've loaded the template.
+        // You can even do stuff in your console for some REPL action. (Use the
+        // art variable instead of svg, though.)
+        // You have access to any variables not proceeded with the var
+        // declaration (i.e. almost all of them right now).
+        topToBottom = svg.gradient(
+            Snap.format("L({x1}, {y1}, {x2}, {y2}){c1}-{c2}", {
+                        x1: 0,
+                        y1: 0,
+                        x2: 0,
+                        y2: height,
+                        c1: "#e6ab5a",
+                        c2: "#51388F"
         }));
+
+        bottomToTop = svg.gradient(
+            Snap.format("L({x1}, {y1}, {x2}, {y2}){c1}-{c2}", {
+                        x1: 0,
+                        y1: 0,
+                        x2: 0,
+                        y2: height,
+                        c1: "#fff",
+                        c2: "#222"
+        }));
+
+        // Background gradient
+        square = svg.rect(0, 0, width, height);
         square.attr({fill: topToBottom});
 
-        bottomToTop = b.gradient(Snap.format("L({x1}, {y1}, {x2}, {y2}){c1}-{c2}", {
-            x1: 0,
-            y1: 0,
-            x2: 0,
-            y2: h,
-            c1: "#000",
-            c2: "#fff"
-        }));
+        // Path string for the XS leggings template outline. Helps remove paths
+        // that aren't completely on the leggings (so seams are less noticable).
+        xsPath = s.select("#Guides #Sew_Lines").toString();
 
-        // Various things on top of the gradient.
+        // Other layers on top of the background. Uncomment to see what they do.
 
-        // for (var i = 200; i >= 0; i--) {
-        //     var c = b.circle(Math.random() * w, Math.random() * h, Math.random() * 100);
-        //     var greyVal = Math.random() * 255;
-        //     var color = Snap.rgb(greyVal, greyVal, greyVal);
-        //     c.attr({ fill: bottomToTop, class: 'circly'});
-        // };
-
-        var width = 20;
-        for (var i = 0; i < 30; i++) {
-            var y = (i / 31) * h;
-            b.line(0, y, w, y).
-                attr({
-                    strokeWidth: width,
-                    stroke: bottomToTop
-                });
-        };
-
-        // var twentieth = w / 24;
+        // Stripes.
+        // // You can type stripes into the console to add more shapes to the group.
+        // stripes = svg.g();
+        // var fraction = width / 24;
         // for (var i = 0; i < 24; i++) {
-        //     var x = i * twentieth;
-        //     var width = twentieth / 2;
-        //     b.rect(x, 0, width, h);
+        //     var x = i * fraction;
+        //     var lineWidth = fraction / 2;
+        //     stripes.rect(x, 0, lineWidth, height);
         // };
+
+        // Text.
+        // shells = svg.text(500, 500, "#SHELLS")
+        //     .attr({
+        //         fontSize: "100"
+        //     });
+
     }
+
+    // Download helpers.
+    function downloadSVG () {
+        window.open('data:text/xml;charset=utf-8,' + escape(art.toString()));
+    }
+
+    document.getElementById('download').addEventListener('click', downloadSVG);
 };
