abdf18a0dd16e56f4d698c4f824a9f7128ec5cf1
Lucas S Eggers <l.eggers91@gmail.com>
2015-01-09 00:11:58 -0800

    rewrote to be friendlier

--- js/main.js
+++ js/main.js
@@ -1,72 +1,22 @@
-function setup () {
-    // Artboad setup:
-    // This works by layering two <svg>s on top of each other. One is the
-    // artboard, the other is the template. In SVG, every node is a layer with
-    // conflicts resolved from top to bottom. If you want that to be useful, you
-    // have to get good at using SVG's <g> node to create layers, or use them as
-    // masks or fills or other fun things. Or lay down your shapes in the right
-    // order ;-)
+// Hello!
+// Welcome to Bombsheller's Look Labs. You can design leggings with code here.
+// If you haven't yet, please read our Designing for Bodies page here:
+// http://bombsheller.com/designing-for-bodies.html.
 
-    // We load the template SVG and set up our environment once we get its width
-    // and height.
-    Snap.load('assets/S12-01.svg', function (fragment) {
+// How to start coding your design:
+// 1) Open a shell (Terminal on Mac).
+// 2) cd to the look-labs folder.
+// 3) Type python -m SimpleHTTPServer.
+// 4) Open localhost:8000.
+// 5) Change the code below.
+// 6) Refresh. goto 5.
 
-        var getDimension = function (fragment, dimensionName) {
-            var value = fragment.node.firstElementChild.attributes.getNamedItem(dimensionName).value;
-            return parseFloat(value);
-        };
-        width = getDimension(fragment, "width");
-        height = getDimension(fragment, "height");
-
-        makeButton = document.getElementById('make');
-        makeButton.style.marginLeft = width + 'px';
-        makeButton.style.fontSize = '100px';
-        downloadLink = document.getElementById('download');
-        downloadLink.style.marginLeft = width + 'px';
-        downloadLink.style.fontSize = '100px';
-
-        // First <svg>. Needs to be added to the DOM first to be below the template.
-        brush = Snap(width, height)
-            .attr({
-                id: 'draw',
-                style:"position: absolute;left: 0;"
-            });
-
-        // Now we add template <svg> to the DOM.
-        s = Snap(width, height)
-            .attr({ style:"position: absolute;left: 0;" })
-            .append(fragment);
-
-        draw(brush, width, height);
-    });
-
-    // Download helpers.
-    function makeSVG () {
-        var art = new Blob([brush.toString()], {type: 'text/svg'});
-        var artURL = window.URL.createObjectURL(art);
-        downloadLink.href = artURL;
-        downloadLink.style.display = 'block';
-    }
-
-    document.getElementById('make').addEventListener('click', makeSVG, false);
-}
-
-window.onload = setup;
-
-// Here's where you get to play with the artboard! We use snap.svg.js to
-// make these shapes (for now; the library could be substituted provided it
-// generates an SVG). Here are its docs: http://snapsvg.io/docs. You also
-// will likely want to know about SVG. Normally when I want to know I just
-// Google or cmd + F on http://www.w3.org/TR/SVG/paths.html or other SVG docs
-// (Mozilla's are nice).
-// Maybe D3 could be fun to use: http://d3js.org/.
-
-// WHEN YOU ARE READY TO SAVE YOUR DESIGN:
+// When you are ready to save your design:
 // 1) Click the "Make SVG" button to the right.
 // 2) Click the "Download SVG" link that just appeared.
 // 3) Test your design by dragging it directly into Artemix.bombsheller.com
 // 4) Tweak some variables for some variants (or something), repeat 1-3.
-//    Make sure you keep a copy of the draw() method somehow! I use git
+//    Make sure you keep a copy of the draw() method somehow! We like git
 //    branches. Copy-pasting this file into a folder with the design works, too.
 // 5) Submit @ http://bombsheller.com/submit-designs.html
 
@@ -78,18 +28,21 @@ window.onload = setup;
 // 2880 units / 40 in = 72 units / inch ~ 28 units / cm.
 function draw (brush, width, height) {
 
+    // Here's where we get to play with the artboard! We use the snap.svg.js
+    // library to draw. Here are its docs: http://snapsvg.io/docs.
+    // If you're an expert in d3 or some other SVG-generating library, feel free
+    // to use that! You also will likely want to know about SVG. Normally when I
+    // want to know I just Google or cmd + F on
+    // http://www.w3.org/TR/SVG/paths.html or other SVG docs (Mozilla's are nice).
+
     // Since a lot of fun things come out of pseudorandom numbers, being able
-    // to control the random seed is important. We use the randomseed library
-    // found here to do so: https://github.com/davidbau/seedrandom.
+    // to control the random number generator is important. We use the randomseed
+    // library found here to do so: https://github.com/davidbau/seedrandom. If
+    // you use Math.random(), changing the input here will change your design
+    // subtly.
     Math.seedrandom('bombsheller');
 
-    // Start by making a couple of gradients because vector gradients rock.
-    // This layer appears below the template because the brush variable was
-    // added to the DOM before the template SVG (see above method).
-    // You can even do stuff in your console for some REPL action using the
-    // using the brush variable to draw things!
-    // You will have access to any variables not proceeded with the var
-    // declaration (i.e. almost all of them right now).
+    // We start by making a couple of gradients because vector gradients rock.
     topToBottom = brush.gradient(
         Snap.format("L({x1}, {y1}, {x2}, {y2}){c1}-{c2}", {
                     x1: 0,
@@ -137,6 +90,72 @@ function draw (brush, width, height) {
     //     });
 
 }
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Plumbing is below. If you're curious about how Look Labs works, read on!
+//
+////////////////////////////////////////////////////////////////////////////////
+
+function setup () {
+    // Artboad setup:
+    // This works by layering two <svg>s on top of each other. One is the
+    // artboard (painted on with the brush variable), the other is the template.
+    // In SVG, every node is a layer with conflicts resolved from bottom to top.
+    // That is, things painted last are on top, just like in RL!
+    // SVG's <g> node can be useful to create layers, but isn't always necessary.
+
+    // We load the template SVG and set up our environment once we get its width
+    // and height.
+    Snap.load('assets/S12-01.svg', function (fragment) {
+
+        var getDimension = function (fragment, dimensionName) {
+            var value = fragment.node.firstElementChild.attributes.getNamedItem(dimensionName).value;
+            return parseFloat(value);
+        };
+        width = getDimension(fragment, "width");
+        height = getDimension(fragment, "height");
+
+        // Artboard creation.
+        // This layer appears below the template because the brush variable is
+        // added to the DOM before the template SVG.
+        // You can even use brush in your console for some REPL painting action!
+        // You will have access to any variables not proceeded with the var
+        // declaration (i.e. almost all of them right now).
+        brush = Snap(width, height)
+            .attr({
+                id: 'draw',
+                style:"position: absolute;left: 0;"
+            });
+
+        // Now we add template <svg> to the DOM.
+        s = Snap(width, height)
+            .attr({ style:"position: absolute;left: 0;" })
+            .append(fragment);
+
+        // Positioning of download button and link.
+        makeButton = document.getElementById('make');
+        makeButton.style.marginLeft = width + 'px';
+        makeButton.style.fontSize = '100px';
+        downloadLink = document.getElementById('download');
+        downloadLink.style.marginLeft = width + 'px';
+        downloadLink.style.fontSize = '100px';
+
+        draw(brush, width, height);
+    });
+
+    // Download helpers.
+    function makeSVG () {
+        var art = new Blob([brush.toString()], {type: 'text/svg'});
+        var artURL = window.URL.createObjectURL(art);
+        downloadLink.href = artURL;
+        downloadLink.style.display = 'block';
+    }
+
+    document.getElementById('make').addEventListener('click', makeSVG, false);
+}
+
+window.onload = setup;
 
 ////////////////////////////////////////////////////////////////////////////////
 //
